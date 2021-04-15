2cdecb6df0a352025b598bb58ea3847988c20681
nicolasg2134 <nicolasg2134@gmail.com>
2021-04-15 15:14:34 +0000

    shell fix

--- builtin.c
+++ builtin.c
@@ -1,12 +1,8 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
  * checkbin - checks if the input
-=======
  * checkbin - checks if the input is a built in and executes it
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
- *
  * @input: input
  * @tokens: tokens
  * Return: 0 if succed, -1 if built in doesn´t exist
@@ -16,15 +12,10 @@ int checkbin(char *input, char **tokens)
 {
 	int a, b;
 	built_in bi[] = {
-<<<<<<< HEAD
 		{func_env, "env"},
-		{func_exit, "exit"},
-=======
-		{fenv, "env"},
-		{fexit, "exit"},
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
+		{func_exit, "exit"),
 		{NULL, NULL}
-	};
+};
 
 	for (a = 0; bi[a].name != NULL; a++)
 	{
@@ -39,20 +30,14 @@ int checkbin(char *input, char **tokens)
 }
 
 /**
-<<<<<<< HEAD
- * func_env - prints enviromental variables
-=======
- * fenv - prints enviromental variables
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
+ * func_env - prints enviromental variable
  *
  * Return: 0
  */
 
-<<<<<<< HEAD
+
 int func_env(void)
-=======
 int fenv(void)
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
 {
 	int a;
 	size_t b = 0;
@@ -67,20 +52,14 @@ int fenv(void)
 }
 
 /**
-<<<<<<< HEAD
  * func_exit - exits the shell
-=======
- * fexit - exits the shell
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
  *
  * Return: 1
  */
 
-<<<<<<< HEAD
-int func_exit(void)
-=======
+
+int func_exit(void);
 int fexit(void)
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
 {
 	return (1);
 }
--- shell.h
+++ shell.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#ifndef SHEll_H
-=======
 #ifndef SHELL_H
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
 #define SHELL_H
 
 #include <stdio.h>
@@ -23,13 +19,8 @@ int _strcmp(char *src1, char *src2);
 int _strlen(char *str);
 char *checkexec(char **file);
 int checkbin(char *input, char **tokens);
-<<<<<<< HEAD
 int func_exit(void);
 int func_env(void);
-=======
-int fexit(void);
-int fenv(void);
->>>>>>> 391e89503915cd3874b29b53f780296d7bd0b3f7
 
 /**
  * struct builtin - built in of the shell
